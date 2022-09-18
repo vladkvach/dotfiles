88ad06d1bd88fc56cb385334ab4626386f603fe5
Vladislav Kvach <kvachvladislav@gmail.com>
2022-09-18 11:36:37 -0700

    add some plugs

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,30 +6,30 @@ end
 
 vim.g.mapleader = ' ' -- TODO: move this into mappings
 local options = {
-    termguicolors = true,           -- Enable 24 bits color in terminal
-    guicursor = "a:block,i-ci-ve:ver25",  -- Set the gui cursor to nothing for each mode
-    inccommand = "nosplit",         -- Show effect of command incrementally while writing it (substitution)
-    mouse = "a",                    -- Enable mouse in all mode
-    tabstop = 4,                    -- A tab is 4 spaces (display)
-    shiftwidth = 4,                 -- Indent to 4 spaces
-    softtabstop = 4,                -- A tab is 4 spaces (insert mode)
-    expandtab = true,               -- Use spaces instead of tab character
-    smartindent = true,             -- Smart auto indent on new line
-    clipboard = "unnamedplus",      -- Use + register for yank, delete, change and put operation
-    hidden = true,                  -- Make sure hidden buffer are not unloaded
-    backup = false,                 -- Disable file backups
-    writebackup = false,            -- Disable file backup on save
-    swapfile = false,               -- Disable swap files
-    laststatus = 3,                 -- Single status line for the whole frame
+    termguicolors = true, -- Enable 24 bits color in terminal
+    guicursor = "a:block,i-ci-ve:ver25", -- Set the gui cursor to nothing for each mode
+    inccommand = "nosplit", -- Show effect of command incrementally while writing it (substitution)
+    mouse = "a", -- Enable mouse in all mode
+    tabstop = 4, -- A tab is 4 spaces (display)
+    shiftwidth = 4, -- Indent to 4 spaces
+    softtabstop = 4, -- A tab is 4 spaces (insert mode)
+    expandtab = true, -- Use spaces instead of tab character
+    smartindent = true, -- Smart auto indent on new line
+    clipboard = "unnamedplus", -- Use + register for yank, delete, change and put operation
+    hidden = true, -- Make sure hidden buffer are not unloaded
+    backup = false, -- Disable file backups
+    writebackup = false, -- Disable file backup on save
+    swapfile = false, -- Disable swap files
+    laststatus = 3, -- Single status line for the whole frame
 }
 
 local window_options = {
-    signcolumn = "yes",             -- Show the sign colum (for gitsigns)
-    number = true,                  -- Show line numbers
-    relativenumber = true,          -- Use relative line numbers
-    scrolloff = 8,                  -- Keep at least 8 lines above the cursor.
-    wrap = false,                   -- Disable line wrap
-    colorcolumn = '120'             -- Color the 120 character as a limit for line length
+    signcolumn = "yes", -- Show the sign colum (for gitsigns)
+    number = true, -- Show line numbers
+    relativenumber = true, -- Use relative line numbers
+    scrolloff = 8, -- Keep at least 8 lines above the cursor.
+    wrap = false, -- Disable line wrap
+    colorcolumn = '120' -- Color the 120 character as a limit for line length
 }
 
 local global_options = {
---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,27 @@
+local present, _ = pcall(require, "packerInit")
+local packer
+
+if present then
+    packer = require "packer"
+else
+    return false
+end
+
+local use = packer.use
+
+return packer.startup({
+    function()
+        use "sindrets/diffview.nvim"
+
+    end,
+    config = {
+        display = {
+            open_fn = function()
+                return require("packer.util").float { border = "single" }
+            end
+        },
+        git = {
+            clone_timeout = 600
+        }
+    },
+})
