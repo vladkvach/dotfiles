18e22cf6a8cf4be5befe915d66438d77139b7cf3
Vladislav Kvach <kvachvladislav@gmail.com>
2022-09-24 06:28:09 -0700

    fix color scheme

--- .config/suckless/st/config.def.h
+++ .config/suckless/st/config.def.h
@@ -95,48 +95,43 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-
 	/* 8 normal colors */
-	[0] = "#202020", /* black   */
-	[1] = "#c13543", /* red     */
-	[2] = "#81957c", /* green   */
-	[3] = "#f9bb80", /* yellow  */
-	[4] = "#356579", /* blue    */
-	[5] = "#8b6397", /* magenta */
-	[6] = "#005d83", /* cyan    */
-	[7] = "#909090", /* white   */
+	"black",
+	"red3",
+	"green3",
+	"yellow3",
+	"blue2",
+	"magenta3",
+	"cyan3",
+	"gray90",
 
 	/* 8 bright colors */
-	[8] = "#606060",  /* black   */
-	[9] = "#d14548",  /* red     */
-	[10] = "#a7b79a", /* green   */
-	[11] = "#fae3a0", /* yellow  */
-	[12] = "#7491a1", /* blue    */
-	[13] = "#87314e", /* magenta */
-	[14] = "#0f829d", /* cyan    */
-	[15] = "#fff0f0", /* white   */
+	"gray50",
+	"red",
+	"green",
+	"yellow",
+	"#5c5cff",
+	"magenta",
+	"cyan",
+	"white",
 
-	/* special colors */
-	[256] = "#101010", /* background */
-	[257] = "#d2c5bc", /* foreground */
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#cccccc",
+	"#555555",
+	"gray90", /* default foreground colour */
+	"black",  /* default background colour */
 };
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor
+ * foreground, background, cursor, reverse cursor
  */
-static unsigned int defaultfg = 257;
-static unsigned int defaultbg = 256;
-static unsigned int defaultcs = 257;
+unsigned int defaultfg = 258;
+unsigned int defaultbg = 259;
+unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
-
-/*
- * Colors used, when the specific fg == defaultfg. So in reverse mode this
- * will reverse too. Another logic would only make the simple feature too
- * complex.
- */
-static unsigned int defaultitalic = 7;
-static unsigned int defaultunderline = 7;
 
 /*
  * Default shape of cursor
