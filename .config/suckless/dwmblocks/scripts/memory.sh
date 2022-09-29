2a2b11f1e91f893124be4cf926d65faebb2d3cd4
Vladislav Kvach <kvachvladislav@gmail.com>
2022-09-29 11:59:36 -0700

    add ram status for bsd

--- .config/suckless/dwmblocks/scripts/memory.sh
+++ .config/suckless/dwmblocks/scripts/memory.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-mem="$(free -h | awk '/^Mem:/ {print $3 "/" $2}')"
+mem="$(grep -i 'memory' /var/run/dmesg.boot | awk -F '[^0-9]*' '{print $3}' | awk -v ORS='/' '{getline x; print x;}1' | awk '{print substr($NF, 1, length($NF)-1)}')"
 echo -e "$mem RAM "
