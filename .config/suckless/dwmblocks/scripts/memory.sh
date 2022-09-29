6a107fe22696d3992427f8d75c87646c6ddac8d7
Vladislav Kvach <92576675+vladkvach@users.noreply.github.com>
2022-09-29 12:15:27 -0700

    Update memory.sh

--- .config/suckless/dwmblocks/scripts/memory.sh
+++ .config/suckless/dwmblocks/scripts/memory.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 mem="$(grep -i 'memory' /var/run/dmesg.boot | awk -F '[^0-9]*' '{print $3}' | awk -v ORS='/' '{getline x; print x;}1' | awk '{print substr($NF, 1, length($NF)-1)}')"
-echo -e "$mem RAM "
+echo -e $mem
