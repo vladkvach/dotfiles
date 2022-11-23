bef17db6a977e6d29d89e3d63873716138c0852d
Vladislav Kvach <kvachvladislav@gmail.com>
2022-11-23 01:12:09 -0800

    add c style

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,7 +5,7 @@
 
 (server-start)
 
-(setq package-list '(diminish rainbow-delimiters ws-butler tramp flycheck no-littering))
+(setq package-list '(diminish rainbow-delimiters ws-butler tramp flycheck))
 
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("elpa" . "https://elpa.gnu.org/packages/")
@@ -234,9 +234,6 @@
 (diminish 'ws-butler-mode)
 (diminish 'buffer-face-mode)
 
-;; Use no-littering to automatically set common paths to the new user-emacs-directory
-(require 'no-littering)
-
 ;; Keep customization settings in a temporary file (thanks Ambrevar!)
 (setq custom-file
       (if (boundp 'server-socket-dir)
@@ -272,9 +269,63 @@
 
 (global-auto-revert-mode 1)
 
+; C++ indentation style
+(defconst vk_c_style
+  '((c-electric-pound-behavior   . nil)
+    (c-tab-always-indent         . t)
+    (c-comment-only-line-offset  . 0)
+    (c-hanging-braces-alist      . ((class-open)
+                                    (class-close)
+                                    (defun-open)
+                                    (defun-close)
+                                    (inline-open)
+                                    (inline-close)
+                                    (brace-list-open)
+                                    (brace-list-close)
+                                    (brace-list-intro)
+                                    (brace-list-entry)
+                                    (block-open)
+                                    (block-close)
+                                    (substatement-open)
+                                    (statement-case-open)
+                                    (class-open)))
+    (c-hanging-colons-alist      . ((inher-intro)
+                                    (case-label)
+                                    (label)
+                                    (access-label)
+                                    (access-key)
+                                    (member-init-intro)))
+    (c-cleanup-list              . (scope-operator
+                                    list-close-comma
+                                    defun-close-semi))
+    (c-offsets-alist             . ((arglist-close         .  c-lineup-arglist)
+                                    (label                 . -4)
+                                    (access-label          . -4)
+                                    (substatement-open     .  0)
+                                    (statement-case-intro  .  4)
+                                    (statement-block-intro .  c-lineup-for)
+                                    (case-label            .  4)
+                                    (block-open            .  0)
+                                    (inline-open           .  0)
+                                    (topmost-intro-cont    .  0)
+                                    (knr-argdecl-intro     . -4)
+                                    (brace-list-open       .  0)
+                                    (brace-list-intro      .  4)))
+    (c-echo-syntactic-information-p . t)))
+
 ; CC++ mode handling
 (defun vk_c_hook ()
-; Abbrevation expansion
+
+  ; Set my style for the current buffer
+  (c-add-style "vk_c_style" vk_c_style t)
+
+  ; Additional style stuff
+  (c-set-offset 'member-init-intro '++)
+
+  ; No hungry backspace
+  (c-toggle-auto-hungry-state -1)
+
+  ; Abbrevation expansion
   (abbrev-mode 1)
 
   (defun vk_header_format ()
@@ -327,8 +378,6 @@
 
 (require 'tramp)
 (setq tramp-default-method "ssh"
-      tramp-persistency-file-name (concat no-littering-var-directory "tramp")
-      tramp-auto-save-directory (concat no-littering-var-directory "tramp-autosave")
       tramp-verbose 10
       tramp-shell-prompt-pattern "\\(?:^\\|\r\\)[^]#$%>\n]*#?[]#$%>] *\\(^[\\[[0-9;]*[a-zA-Z] *\\)*")
 (tramp-set-completion-function "ssh" '((tramp-parse-sconfig "/etc/ssh_config")
