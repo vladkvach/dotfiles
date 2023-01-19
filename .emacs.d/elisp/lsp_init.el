5e9e9fcdf9d501b37b4032cfe898792d4ff3f1d2
Vladislav Kvach <92576675+vladkvach@users.noreply.github.com>
2023-01-19 08:39:23 -0800

    Update lsp_init.el

--- .emacs.d/elisp/lsp_init.el
+++ .emacs.d/elisp/lsp_init.el
@@ -1,8 +1,7 @@
 (require 'lsp-mode)
 
-(setq company-idle-delay 0.0
+(setq company-idle-delay 0.1
       company-minimum-prefix-length 1
-      lsp-idle-delay 0.1
       lsp-enable-which-key-integration t
       lsp-headerline-breadcrumb-enable nil
       lsp-headerline-breadcrumb-enable nil
@@ -10,9 +9,9 @@
       lsp-modeline-diagnostics-enable nil
       lsp-semantic-tokens-enable t ;; hide unreachable ifdefs
       ;; LSP UI related tweaks
+      lsp-ui-sideline-enable nil
       lsp-ui-doc-show-with-cursor t
       lsp-ui-doc-show-with-mouse nil
-      lsp-ui-sideline-enable nil
       lsp-ui-sideline-show-hover nil
       lsp-ui-sideline-show-symbol nil
       lsp-ui-sideline-show-diagnostics nil
