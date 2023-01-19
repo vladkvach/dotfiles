d854d9a54aad4fc351e3fae4a4f4b9d37e3c6415
Vladislav Kvach <92576675+vladkvach@users.noreply.github.com>
2023-01-19 08:26:38 -0800

    Update company_init.el

--- .emacs.d/elisp/company_init.el
+++ .emacs.d/elisp/company_init.el
@@ -1,7 +1,7 @@
 (with-eval-after-load 'lsp-mode
     (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
     (require 'company)
-    (add-hook 'after-init-hook 'global-company-mode)
+    (add-hook 'after-init-hook 'global-company-mode))
 
 (with-eval-after-load 'company
   (define-key company-active-map (kbd "TAB") #'company-complete-common-or-cycle))
