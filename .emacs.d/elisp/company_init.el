9e5ac3ea34218e28edf91a75f4181adebe39925b
Vladislav Kvach <92576675+vladkvach@users.noreply.github.com>
2023-01-19 09:59:38 -0800

    Update company_init.el

--- .emacs.d/elisp/company_init.el
+++ .emacs.d/elisp/company_init.el
@@ -4,4 +4,5 @@
     (add-hook 'after-init-hook 'global-company-mode))
 
 (with-eval-after-load 'company
-  (define-key company-active-map (kbd "TAB") #'company-complete-common-or-cycle))
+  (define-key company-active-map (kbd "<tab>") #'company-complete-selection)
+  (define-key lsp-mode-map (kbd "<tab>") #'company-indent-or-complete-common))
